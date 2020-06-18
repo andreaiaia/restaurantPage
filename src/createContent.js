b0ee20428c0f0cb5769ca7dce2e781b34915ba4d
Andrea Bianchi <bianan96@gmail.com>
2020-06-18 11:28:16 +0200

    set up

---
+++ src/createContent.js
@@ -0,0 +1,16 @@
+const setUpPage = () => {
+  const content = document.getElementById('content');
+
+  const logo = document.createElement("img");
+  headline.src = "../img/logo.png";
+  const headline = document.createElement("h1");
+  headline.textContent = "Welcome to the realm of the taste";
+  const text = document.createElement("p");
+  text.textContent = "Behold! Our dishes are cooked with the loving hand of the best chefs, with the best ingredients, served to you by the best waiters Everything, for the best clients possible!";
+
+  content.appendChild(logo);
+  content.appendChild(headline);
+  content.appendChild(text);
+}
+
+export {setUpPage};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import setUpPage from './createContent';
+
+setUpPage();
+
+console.log('hello');
